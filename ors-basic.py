d89bc3526146f5b73773b8e0e634e1b7f30ef76b
mervekeskin <mervsen@gmail.com>
2023-12-30 17:15:55 +0300

    initial commit

--- ors-basic.py
+++ ors-basic.py
@@ -14,46 +14,94 @@ import os
 load_dotenv()
 ORS_API_KEY=os.getenv('ORS_API_KEY')
 
+
 def loc_to_string(location):
+    #hesaplama sonrası haritada işaretleme için lokasyonların ters çevrilmesi gerekiyor
     return (str(location[1]) + ',' + str(location[0]))
-    
+
 def create_googlemaps_link(steps):
     step_num=len(steps)
     googlelink="https://www.google.com/maps/dir/?api=1&travelmode=driving&origin="+ loc_to_string(steps[0]['location']) +'&destination='+ loc_to_string(steps[step_num-1]['location'])+'&waypoints='
     for i in range(1,step_num-1):
         googlelink+=loc_to_string(steps[i]['location'])+'|'
     return googlelink[:-1]
-    
+
 def setData():
+    
     data = {} 
+    
+    #örnek lokasyonlar
+
+    locations= [ 
+        [41.034949993918524, 29.031396215507783], #0-kuzguncuk bostanı 
+        [41.034415862984325, 29.02950794048176], #1-menteş sokak, üsküdar
+        [41.030531143952444, 29.025430983215802], #2-fethipaşa korusu
+        [41.02918762529382, 29.02817756507183], #3-üsküdar lisesi
+        [41.02106118095182, 29.038412874048447], #4-capitol avm
+        [41.021903010548584, 29.048970048761184], #5-altunizade metro
+        [41.01430996561763, 29.046652620337948], #6-validebağ korusu
+        [40.99170373971553, 29.021525686851778], #7-kadıköy meydanı
+        [40.99693495276592, 29.018714731983497], #8-haydarpaşa garı
+    ]
+    #google maps lokasyonlarının hesaplama öncesi ters çevrilmesi gerekiyor
+    for loc in locations:
+        loc.reverse()
+    
     data['vehicles_list'] =[
       { 
         "id":1, 
-       "start": [29.038670364584824,41.01234706794124], 
-       "end": [29.038670364584824,41.01234706794124], 
+       "start": locations[0], 
+       "end": locations[0], 
        "capacity": [10]
       },
-      { 
+         { 
         "id":2, 
-       "start": [29.038670364584824,41.01234706794124], 
-       "end": [29.038670364584824,41.01234706794124], 
-       "capacity": [12]
-      }
+       "start": locations[1], 
+       "end": locations[1], 
+       "capacity": [15]
+      },
+        { 
+        "id":3, 
+       "start": locations[2], 
+       "end": locations[2], 
+       "capacity": [15]
+      },
     ]
     data['num_vehicles']=len(data['vehicles_list'])
     data['shipment_list'] = [
       {
-        "pickup": { "location": [29.02725488374569,41.0158766716361 ], "service": 600},
-        "delivery": { "location": [29.01841432339658,41.02108977700994 ], "service": 600},
+        "pickup": { "location": locations[1], "service": 600},
+        "delivery": { "location": locations[4], "service": 600},
           "amount": [10],
           "skills": [],
           "priority": 0
 
       },
       {
-        "pickup": { "location": [29.01253492161102,41.01908229459765], "service": 600}, 
-       "delivery": { "location": [29.012406175586516,41.00914111722123 ], "service": 600},
+        "pickup": { "location": locations[3], "service": 600}, 
+       "delivery": { "location": locations[6], "service": 600},
           "amount": [5],
+          "skills": [],
+          "priority": 0
+      },
+        {
+        "pickup": { "location": locations[2], "service": 600}, 
+       "delivery": { "location": locations[8], "service": 600},
+          "amount": [2],
+          "skills": [],
+          "priority": 0
+      },
+        {
+        "pickup": { "location": locations[4], "service": 600}, 
+       "delivery": { "location": locations[7], "service": 600},
+          "amount": [2],
+          "skills": [],
+          "priority": 0
+      },
+         {
+        "pickup": { "location": locations[0], "service": 600}, 
+       "delivery": { "location": locations[6], "service": 600},
+          "amount": [3],
           "skills": [],
           "priority": 0
       },
@@ -70,7 +118,6 @@ def setData():
         print('  Delivery loc: ', pad['delivery']['location'])
         print('  Amount: ',pad['amount'])
     return data
-
 
 def setOrsRequest(data):
     request={}
@@ -99,6 +146,7 @@ def setOrsRequest(data):
         ))
 
     return request
+
 def print_results(results):
     if not results:
         print('No results')
@@ -140,7 +188,7 @@ def print_results(results):
             print('----- Step ',ids,'------')
             print('- Step type: ', step['type'])
             print('- Step location: ', step['location'])
-            if(step['type']!='start' and step['type']!='end'):
+            if(step['type'] != 'start' and  step['type']!='end'):
                 print('- Arrival time: ', step['arrival'])
                 print('- Setup time (sec): ', step['setup'])
                 print('- Service time (sec): ', step['service'])
@@ -151,11 +199,9 @@ def print_results(results):
                 print('- Violations', step['violations'])
                 #print('- ID of the job', step['id'])
 def main():
-    client = ors.Client(key='5b3ce3597851110001cf6248e520f95e7da847019624418cf392d19e')
+    client = ors.Client(key=ORS_API_KEY)
     data=setData()
-    
     request=setOrsRequest(data)
     optimized_results = client.optimization( vehicles=request['vehicles'], shipments=request['shipments'], geometry=True)
     print_results(optimized_results)
- 
 main()
